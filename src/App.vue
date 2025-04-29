3c66c5c56e87e48481c80c8196dc5c712689bdae
aaqib8 <aaqib.hyd@gmail.com>
2025-04-29 16:24:18 -0300

    feat: add ui library and setup routes

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,19 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import { ref, watch } from 'vue'
+<script setup lang="ts"></script>
 
-let id = 0;
-const newTodo = ref('')
-const hideCompleted = ref(false)
-const todos = ref([
-  { id: id++, text: 'Learn HTML', done: true },
-  { id: id++, text: 'Learn JavaScript', done: true },
-  { id: id++, text: 'Learn Vue', done: false }
-])
-
-watch(todos, () => {
-  console.log('todos: ', todos.value);
-}, { deep: true });
-
-
-function addTodo() {
-  console.log("Add Todo: ",);
-  todos.value.push({ id: id++, text: newTodo.value, done: false })
-  newTodo.value = ''
-}
-
-function removeTodo(id: number) {
-  console.log('id: ', id);
-  todos.value = todos.value.filter(todo => todo.id !== id)
-}
-
-</script>
 <template>
-  <HelloWorld msg="asdf"></HelloWorld>
-  <section class="flex flex-col">
-    <div>
-      <input v-model="newTodo" required placeholder="new todo">
-      <button @click="addTodo">Add Todo</button>
-    </div>
-    <div>
-      <ul>
-        <li v-for="todo in todos" :key="todo.id"> {{ todo.text }} <button @click="removeTodo(todo.id)">X</button>
-        </li>
-      </ul>
-    </div>
-  </section>
+  <nav>
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/todo">Todo</RouterLink>
+    <RouterLink to="/solo">Solo</RouterLink>
+    <RouterLink to="/multi">Multi</RouterLink>
+  </nav>
+  <RouterView />
 </template>
+
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+nav {
+  display: flex;
+  gap: 1rem;
+  margin-bottom: 2rem;
 }
 </style>
