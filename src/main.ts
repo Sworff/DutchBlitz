f126c96f22012da3af7d5727de0243609fbbf44e
aaqib8 <aaqib.hyd@gmail.com>
2025-04-29 16:24:18 -0300

    feat: add ui library and setup routes

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,28 @@
 import { createApp } from 'vue'
+import './roots.css'
 import './style.css'
 import App from './App.vue'
+import { createRouter, createMemoryHistory, type RouteRecordRaw } from 'vue-router'
+import Solo from './pages/Solo.vue'
+import Multi from './pages/Multi.vue'
+import Todo from './components/Todo.vue'
+import HelloWorld from './components/HelloWorld.vue'
+import PrimeVue from 'primevue/config';
 
-createApp(App).mount('#app')
+const routes:RouteRecordRaw[] = [
+  { path: '/', component: HelloWorld },
+  { path: '/todo', component: Todo },
+  { path: '/solo', component: Solo },
+  { path: '/multi', component: Multi },
+]
+
+const router = createRouter({
+  history: createMemoryHistory(),
+  routes,
+})
+
+
+
+createApp(App).use(PrimeVue, {
+  unstyled: true
+}).use(router).mount('#app')
