8179eda0182ba429523b68703a4c9a9b68159f36
Kapz85 <kacperrymsza@gmail.com>
2025-04-29 17:53:18 -0400

    Add fetch call for Multi title

--- src/pages/Multi.vue
+++ src/pages/Multi.vue
@@ -1,7 +1,19 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import axios from 'axios';
+import { ref, onMounted } from 'vue';
+
+const title = ref('')
+
+onMounted(async () => {
+  try {
+    const response = await axios.get('https://localhost:44370/DutchBlitz');
+    title.value = response.data;
+  } catch(error) {
+    title.value = "Multi!"
+  }
+})
+</script>
 
 <template>
-  <h1>MULTI</h1>
+  <h1>{{ title }}</h1>
 </template>
-
-<style scoped></style>
